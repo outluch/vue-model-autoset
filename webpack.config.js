e12a1f71aef77a05444b479eec5a1a63729134d9
outluch <outluch@gmail.com>
2017-05-26 05:08:37 +0400

    Added babel and set webpack to compile with -p

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+        }
+      }
+    ]
   }
 };
